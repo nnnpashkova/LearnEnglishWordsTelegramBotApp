7918761054e8ab1e1d8bc599be37ba8cbeb0d944
Nata <nata.mir.99@mail.ru>
2023-06-12 10:31:56 +0300

    words

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -3,7 +3,16 @@ import java.io.File
 fun main() {
     val wordsFiles = File("words.txt")
     wordsFiles.createNewFile()
+    val lines = wordsFiles.readLines()
     for (value in wordsFiles.readLines()) {
-        println(value)
+        val line = value.split("|")
+        val word = Word(original = line[0], translate = line[1])
+        println(word)
     }
+
 }
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
