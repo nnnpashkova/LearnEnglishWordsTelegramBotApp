8837d14b2f3e93f791212f151d806a177ed40f40
Nata <nata.mir.99@mail.ru>
2023-06-12 21:54:42 +0300

    words

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -1,18 +1,26 @@
 import java.io.File
 
 fun main() {
+    val dictionary: MutableList<Word> = mutableListOf()
     val wordsFiles = File("words.txt")
     wordsFiles.createNewFile()
     val lines = wordsFiles.readLines()
     for (value in wordsFiles.readLines()) {
         val line = value.split("|")
-        val word = Word(original = line[0], translate = line[1])
-        println(word)
+        val word = Word(
+            original = line[0],
+            translate = line[1],
+            correctAnswersCount = line.getOrNull(2)?.toInt() ?: 0
+        )
+        dictionary.add(word)
     }
-
+    dictionary.forEach {
+        println(it)
+    }
 }
+
 data class Word(
     val original: String,
     val translate: String,
-    val correctAnswersCount: Int = 0,
+    var correctAnswersCount: Int = 0,
 )
