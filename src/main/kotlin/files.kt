97d58b066e181bf8b43854dd6ee2d63df4c4e248
Nata <nata.mir.99@mail.ru>
2023-06-13 17:55:03 +0300

    Implementation of the menu and section with statistics

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -1,6 +1,46 @@
 import java.io.File
 
 fun main() {
+    while (true) {
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+        when (readln().toInt()) {
+            LEARN_WORDS -> println("Выбрали 1")
+            STATISTICS -> {
+                println("Выбрали 2")
+                computeStatistics()
+            }
+
+            EXIT -> break
+        }
+    }
+}
+
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0,
+)
+
+fun computeStatistics() {
+    val dictionary = readDictionaryFromFile()
+    val sizeWords = dictionary.size
+    var learnedWords = 0
+
+    dictionary.forEach { word ->
+        if (word.correctAnswersCount >= MINIMUM_CORRECT_ANSWERS) {
+            learnedWords++
+        }
+    }
+    val percentageLearnedWords = (learnedWords * 100) / sizeWords
+    println(
+        "Выучено: $learnedWords, из: $sizeWords|$percentageLearnedWords"
+    )
+    dictionary.forEach {
+        println(it)
+    }
+}
+
+fun readDictionaryFromFile(): List<Word> {
     val dictionary: MutableList<Word> = mutableListOf()
     val wordsFiles = File("words.txt")
     wordsFiles.createNewFile()
@@ -13,13 +53,10 @@ fun main() {
         )
         dictionary.add(word)
     }
-    dictionary.forEach {
-        println(it)
-    }
+    return dictionary
 }
 
-data class Word(
-    val original: String,
-    val translate: String,
-    var correctAnswersCount: Int = 0,
-)
+const val LEARN_WORDS = 1
+const val STATISTICS = 2
+const val EXIT = 0
+const val MINIMUM_CORRECT_ANSWERS = 3
