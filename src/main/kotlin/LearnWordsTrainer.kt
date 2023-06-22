0d560c1a89cc10dfe5d505ce3e3f6dab0a9057ac
Nata <nata.mir.99@mail.ru>
2023-06-22 17:55:28 +0300

    Refactoring and testing - fix

--- src/main/kotlin/LearnWordsTrainer.kt
+++ src/main/kotlin/LearnWordsTrainer.kt
@@ -11,10 +11,19 @@ data class Question(
     val correctAnswer: Word,
 )
 
-class LearnWordsTrainer {
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0,
+)
+
+class LearnWordsTrainer(private val learnedAnswerCount:Int = 3,
+                        private val countOfQuestionWords: Int = 4,) {
+
+    private var question: Question? = null
     val dictionary = readDictionaryFromFile()
 
-    fun saveDictionary(dictionary: List<Word>) {
+    private fun saveDictionary(dictionary: List<Word>) {
         val writer = File("words.txt").bufferedWriter()
         dictionary.forEach {
             val line = "${it.original}|${it.translate}|${it.correctAnswersCount}"
@@ -26,54 +35,61 @@ class LearnWordsTrainer {
 
     fun getStatistics(): Statistics {
         val sizeWords = dictionary.size
-        val learnedWords = dictionary.filter { it.correctAnswersCount >= MINIMUM_CORRECT_ANSWERS }.size
+        val learnedWords = dictionary.filter { it.correctAnswersCount >=3}.size
         val percentageLearnedWords = (learnedWords * 100) / sizeWords
         return Statistics(sizeWords, learnedWords, percentageLearnedWords)
     }
 
     fun getNextQuestion(): Question? {
-        val unLearnedWords = dictionary.filter { it.correctAnswersCount < MINIMUM_CORRECT_ANSWERS }
+        val unLearnedWords = dictionary.filter { it.correctAnswersCount < learnedAnswerCount }
         if (unLearnedWords.isEmpty()) return null
         val words = unLearnedWords.shuffled()
             .toMutableSet()
-            .take(NUMBER_OF_ANSWERS_TO_THE_QUESTION)
+            .take(countOfQuestionWords)
             .toMutableSet()
-        if (words.size < NUMBER_OF_ANSWERS_TO_THE_QUESTION) {
-            val additionalWordsCount = NUMBER_OF_ANSWERS_TO_THE_QUESTION - words.size
+        if (words.size < countOfQuestionWords) {
+            val additionalWordsCount = countOfQuestionWords - words.size
             val additionalWords = dictionary.minus(words)
                 .shuffled()
                 .take(additionalWordsCount)
             words.addAll(additionalWords)
         }
         val correctAnswer = words.random()
-        return Question(
+        question = Question(
             variants = words.toMutableList(),
             correctAnswer = correctAnswer
         )
+        return question
     }
 
-    private fun readDictionaryFromFile(): List<Word> {
-        val wordsFiles = File("words.txt")
-        return wordsFiles.readLines().mapNotNull { value ->
-            val line = value.split("|")
-            if (line.size == WORD_REQUIRED_FIELDS_COUNT) {
-                Word(
-                    original = line[0],
-                    translate = line[1],
-                    correctAnswersCount = line.getOrNull(2)?.toInt() ?: 0
-                )
+    fun checkAnswer(userAnswerIndex: Int?): Boolean {
+        return question?.let {
+            if (question == null) return false
+            val correctAnswerId = it.variants.indexOf(it.correctAnswer)
+            if (correctAnswerId == userAnswerIndex) {
+                it.correctAnswer.correctAnswersCount++
+                saveDictionary(dictionary)
+                true
             } else {
-                println("Ошибка: файл содержит строку неправильного формата: $line")
-                null
+                false
             }
-        }
+        } ?: false
     }
 }
 
-
-
-
-
-
-
-
+private fun readDictionaryFromFile(): List<Word> {
+    val wordsFiles = File("words.txt")
+    return wordsFiles.readLines().mapNotNull { value ->
+        val line = value.split("|")
+        if (line.size == 3) {
+            Word(
+                original = line[0],
+                translate = line[1],
+                correctAnswersCount = line.getOrNull(2)?.toInt() ?: 0
+            )
+        } else {
+            println("Ошибка: файл содержит строку неправильного формата: $line")
+            null
+        }
+    }
+}
--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -1,57 +1,44 @@
 
-data class Word(
-    val original: String,
-    val translate: String,
-    var correctAnswersCount: Int = 0,
-)
+fun Question.asConsoleString(): String {
+    val variants = this.variants
+        .mapIndexed{ index: Int, word: Word -> "${index + 1} - ${word.translate}"  }
+        .joinToString(separator = "\n" )
+    return this.correctAnswer.original + "\n" + variants + "\n0 - выйти в меню"
+}
 
 fun main() {
-    val trainer = LearnWordsTrainer()
+    val trainer = LearnWordsTrainer(3, 4)
+
     while (true) {
-        println("Меню: $LEARN_WORDS – Учить слова, $STATISTICS – Статистика, $EXIT – Выход")
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
         when (readln().toIntOrNull()) {
-            LEARN_WORDS -> {
+            1 -> {
                 while (true) {
                     val question = trainer.getNextQuestion()
                     if (question == null) {
                         println("Все слова выучены")
                         break
-                    }
-                    println("Как переводится слово ${question.correctAnswer.original}")
-                    println("Варианты ответа:")
-                    question.variants.forEachIndexed { index, value -> println("${index + 1} - ${value.translate}") }
-                    println("$EXIT - выйти в меню")
-                    println("Введите ответ:")
-                    when (readln().toInt()) {
-                        question.variants.indexOf(question.correctAnswer) + 1 -> {
-                            question.correctAnswer.correctAnswersCount++
-                            trainer.saveDictionary(trainer.dictionary)
-                            println("Верно ${question.correctAnswer.translate}")
-                        }
-                        EXIT -> {
-                            return
-                        }
-                        else -> {
-                            println("Неверно.")
+                    } else {
+                        println(question.asConsoleString())
+                        val userAnswerInput = readln().toIntOrNull()
+                        if (userAnswerInput == 0) break
+
+                        if (trainer.checkAnswer(userAnswerInput?.minus(1))) {
+                            println ("Правильно! - ${question.correctAnswer.translate}")
+                        } else {
+                            println("Неправильно!")
                         }
                     }
                 }
             }
 
-            STATISTICS -> {
-              val statistics = trainer.getStatistics()
+            2 -> {
+                val statistics = trainer.getStatistics()
                 println("Выучено: ${statistics.learnedWords} из ${statistics.sizeWords} | ${statistics.percentageLearnedWords}%")
             }
 
-            EXIT -> break
+            3 -> break
             else -> println("Введите 1,2 или 0")
         }
     }
 }
-
-const val LEARN_WORDS = 1
-const val STATISTICS = 2
-const val EXIT = 0
-const val MINIMUM_CORRECT_ANSWERS = 3
-const val NUMBER_OF_ANSWERS_TO_THE_QUESTION = 4
-const val WORD_REQUIRED_FIELDS_COUNT = 3
